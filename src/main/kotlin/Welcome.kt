8a204864d99e5f26d36369b5bdad7ae77e20765d
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-03-31 23:42:40 +0530

    Experimenting coroutines examples

--- src/main/kotlin/Welcome.kt
+++ src/main/kotlin/Welcome.kt
@@ -1,14 +1,18 @@
 import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin !")
-    val count = 100
-    println("Thread started from ")
+    //println("Welcome to Kotlin !")
+    //val count = 100
+    println("Thread started from ${Thread.currentThread().name}")
     thread {
-        println("Thread executing from ${Thread.currentThread().name}")
-        for(i in 1..count) {
+        println("Thread started from ${Thread.currentThread().name}")
+        // Simulated the long running operation
+        Thread.sleep(1000)
+       /* for(i in 1..count) {
             println("Downloading ${i}/${count}")
-        }
+        }*/
+        println("Thread completed from ${Thread.currentThread().name}")
     }
-    println("Thread executing from ${Thread.currentThread().name}")
+    //Thread.sleep(1000)
+    println("Thread completed from ${Thread.currentThread().name}")
 }
