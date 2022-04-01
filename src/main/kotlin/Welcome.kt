5da1350fac7010cefbedd3af351da44e2e31b07d
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-04-01 20:55:21 +0530

    Experimenting coroutines examples

--- src/main/kotlin/Welcome.kt
+++ src/main/kotlin/Welcome.kt
@@ -1,18 +1,23 @@
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
     //println("Welcome to Kotlin !")
     //val count = 100
     println("Thread started from ${Thread.currentThread().name}")
-    thread {
+    val deferredJob: Deferred<Int> = async(Dispatchers.IO) {
         println("Thread started from ${Thread.currentThread().name}")
+        println("${Thread.currentThread().name} is processing...")
         // Simulated the long running operation
-        Thread.sleep(1000)
-       /* for(i in 1..count) {
-            println("Downloading ${i}/${count}")
-        }*/
+        delay(10000)
+        /* for(i in 1..count) {
+             println("Downloading ${i}/${count}")
+         }*/
         println("Thread completed from ${Thread.currentThread().name}")
+        15
     }
-    //Thread.sleep(1000)
+    //Thread.sleep(2000)
+    deferredJob.join()
+    println("Deferred job : ${deferredJob.await()}")
     println("Thread completed from ${Thread.currentThread().name}")
 }
