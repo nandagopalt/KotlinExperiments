c4eb9fbf01a01ab469b932b9e2bbc071f00ada39
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-04-12 23:52:13 +0530

    Factory pattern usage

--- src/main/java/designpatterns/factorypattern/NotificationService.java
+++ src/main/java/designpatterns/factorypattern/NotificationService.java
@@ -2,6 +2,7 @@ package designpatterns.factorypattern;
 
 public class NotificationService {
     public static void main(String args[]) {
+        // Let subclass decide which object to be created based on the input parameter passed to the factory method
         Notification notificationService;
         notificationService = NotificationFactory.createNotification("SMS");
         notificationService.notifyUser();
