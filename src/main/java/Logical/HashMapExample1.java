38f16c9164c71746d0c20cc92782f45281fb9d1c
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-05-26 23:08:36 +0530

    HashMap Example 1

---
+++ src/main/java/Logical/HashMapExample1.java
@@ -0,0 +1,14 @@
+package Logical;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample1 {
+    public static void main(String args[]) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        System.out.println(map);
+    }
+}
