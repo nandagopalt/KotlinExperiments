3cf9e0ba0a04af3c9f8f11328f099e2f3c676900
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-05-27 23:40:57 +0530

    HashMap Example | Iterating the hashmap

--- src/main/java/Logical/HashMapExample1.java
+++ src/main/java/Logical/HashMapExample1.java
@@ -9,6 +9,23 @@ public class HashMapExample1 {
         map.put(1, "One");
         map.put(2, "Two");
         map.put(3, "Three");
+        map.put(null, null);
+        map.put(null, "null");
+        map.put(4, null);
+        //map.put(4, "Four");
+        map.put(5, "Five");
         System.out.println(map);
+        // Iterating the hashmap through entry set
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("Key from Entry :" + entry.getKey() + "," + "Value from Entry :" + entry.getValue());
+        }
+        // Iterating the hashmap through key set
+        for (Integer key : map.keySet()) {
+            System.out.println("Key :" + key);
+        }
+        // Iterating the hashmap through values
+        for (String value : map.values()) {
+            System.out.println("Value : " + value);
+        }
     }
 }
